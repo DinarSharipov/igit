a2274444d683a06ba7130c186feb120df7e31f7c
DinarSharipov <82703414+DinarSharipov@users.noreply.github.com>
2025-03-03 11:07:30 +0000

    Deploying to gh-pages from @ DinarSharipov/igit@73cb2f034527d4eaac505b499adbd2af389e6340 🚀

--- src/components/ProtectedRoute.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from "react";
-import { Navigate } from "react-router-dom";
-
-export const ProtectedRoute: React.FC<{ children: ReactNode }> = ({ children }) => {
-  return localStorage.getItem("token") ? children : <Navigate to="/login" replace />;
-};
